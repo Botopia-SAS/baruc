0d43daf634982f28a243d890c29ae97c5c1b70dd
IsaacPaez <isaacpaz0123@gmail.com>
2025-05-16 16:30:44 -0500

    ajuste page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Ventajas from "../components/Ventajas/page";
 
 export default function Home() {
   return (
-    <main className="relative z-10">
+    <main className="mx-auto max-w-screen-xl px-4 md:px-8 lg:px-16 text-white relative z-10">
       <Navbar />
       <Hero />
       <Demo />
